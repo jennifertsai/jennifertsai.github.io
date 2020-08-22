db5e068f32dfa3c723f7c89a04ad8d802847378d
Jennifer Tsai <jennifer.tsai11@gmail.com>
2020-08-22 00:56:21 -0700

    added gallery page, imported photos

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ $(function () {
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
   });
+
+  $(document).ready(function () {
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox({
+      alwaysShowClose: true,
+      showArrows: true,
+    });
+    });
+  });
